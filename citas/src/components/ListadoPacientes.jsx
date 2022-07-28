7ed4a4c87ad7c4ad09df3dde5ee768abe0125d25
Guido-87 <alvarezguido87@gmail.com>
2022-07-28 01:46:31 -0300

    App Citas

---
+++ citas/src/components/ListadoPacientes.jsx
@@ -0,0 +1,40 @@
+import Paciente from "./Paciente"
+
+const ListadoPacientes = ({ pacientes, setPaciente, eliminarPaciente }) => {
+
+    return (
+        <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+            {pacientes && pacientes.length > 0 ? (
+                <>
+                    <h2 className="font-blac text-3xl text-center">
+                        Listado Pacientes
+                    </h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Administra tus {''}
+                        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+                    </p>
+                    {pacientes.map((paciente) => (
+                        <Paciente
+                            paciente={paciente}
+                            key={paciente.id}
+                            setPaciente={setPaciente}
+                            eliminarPaciente={eliminarPaciente}
+                        />
+                    ))}
+                </>
+            ) :
+                <>
+                    <h2 className="font-blac text-3xl text-center">
+                        No hay Pacientes
+                    </h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Comienza agregando pacientes {''}
+                        <span className="text-indigo-600 font-bold">y aparecerán acá</span>
+                    </p>
+                </>}
+
+        </div>
+    )
+}
+
+export default ListadoPacientes
