7ff3d495077ebb1ebdeaa066a106a5b4800004e0
Guido-87 <alvarezguido87@gmail.com>
2022-07-28 01:57:49 -0300

    Strapi

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6708b6f3bd89274ab1ba4ab087d244c0'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '43622360-b2ef-4be1-bba7-a2345d2d71b4'
+};
