7fb6297bba157f95456993fe586a8dce247199e5
Guido Alvarez <galvarez@technisys.com>
2023-01-04 06:15:15 -0300

    Tutorial Redux y Sagas

---
+++ redux-saga-tutorial/src/components/NewUserForm.js
@@ -0,0 +1,61 @@
+import { Component } from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+
+class NewUserForm extends Component {
+    state = {
+        firstName: '',
+        lastName: '',
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const {firstName, lastName} = this.state;
+        this.props.onSubmit({
+            firstName,
+            lastName
+        });
+
+        this.setState({
+            firstName: '',
+            lastName: ''
+        });
+    };
+
+    handleFirstNameChange = e => {
+        this.setState({
+            firstName: e.target.value
+        });
+    };
+
+    handleLastNameChange = e => {
+        this.setState({
+            lastName: e.target.value
+        });
+    };
+
+    render() {
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <FormGroup>
+                    <Label>
+                        First name:
+                    </Label>
+                    <Input placeholder="First name" onChange={this.handleFirstNameChange} />
+                </FormGroup>
+                <FormGroup>
+                    <Label>
+                        Last name:
+                    </Label>
+                    <Input placeholder="Last name" onChange={this.handleLastNameChange} />
+                </FormGroup>
+                <FormGroup>
+                    <Button block outline type="submit" color="primary">
+                        Create
+                    </Button>
+                </FormGroup>
+            </Form>
+        );
+    }
+}
+
+export default NewUserForm;
