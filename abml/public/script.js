72364320d40dedf54412e44aa5ead15337acf626
Guido <alvarezguido87@gmail.com>
2024-12-09 18:31:24 -0300

    Refactor script

--- abml/public/script.js
+++ abml/public/script.js
@@ -1,43 +1,30 @@
-let equipo1 = [];
-let equipo2 = [];
+const equipo1 = [];
+const equipo2 = [];
 
 const arqueros = [
-    {nombre: "Edu", posicion: "ARQ", estado: 4},
-    {nombre: "Seba", posicion: "ARQ", estado: 3},
+  { nombre: "Edu", posicion: "ARQ", estado: 4 },
+  { nombre: "Seba", posicion: "ARQ", estado: 3 },
 ];
 
 const jugadores = [
-    {nombre: "Guido", posicion: "DEL", estado: 4},
-    {nombre: "Santi", posicion: "DEL", estado: 4},
-    {nombre: "Leo", posicion: "DEF", estado: 3},
-    {nombre: "Carli", posicion: "DEL", estado: 4},
-    {nombre: "Chara", posicion: "DEF", estado: 4},
-    {nombre: "Eli", posicion: "DEL", estado: 4},
-    {nombre: "Esteban", posicion: "DEF", estado: 4},
-    {nombre: "Pablo", posicion: "DEF", estado: 3},
+  { nombre: "Guido", posicion: "DEL", estado: 4 },
+  { nombre: "Fede", posicion: "DEL", estado: 2 },
+  { nombre: "Leo", posicion: "DEF", estado: 4 },
+  { nombre: "Joaquín", posicion: "DEL", estado: 3 },
+  { nombre: "Agus", posicion: "DEF", estado: 2 },
+  { nombre: "Chapa", posicion: "DEL", estado: 1 },
+  { nombre: "Ale", posicion: "DEF", estado: 4 },
+  { nombre: "Pablo", posicion: "DEF", estado: 3 },
 ];
 
-if (equipo1.length === 0) {
-    const index = Math.floor(Math.random() * arqueros.length);
-    equipo1.push(arqueros[index]);
-    arqueros.splice(index, 1);
-}
-equipo2.push(arqueros[0]);
+equipo1.push(arqueros[0]);
+equipo2.push(arqueros[1]);
 
-let defensores = 0;
-let delanteros = 0;
-for (let i=0 ; i < 4 ; i++) {
-    const index = Math.floor(Math.random() * jugadores.length);
-    if (jugadores[index].posicion === "DEF" && defensores <= 2) {
-        defensores++;
-    }
-    if (jugadores[index].posicion === "DEL" && delanteros <= 2) {
-        delanteros++;
-    }
-    equipo1.push(jugadores[index]);
-    jugadores.splice(index, 1);
-}
-equipo2.push(...jugadores);
+const defensores = jugadores.filter(jugador => jugador.posicion === "DEF").sort(() => Math.random() - 0.5);
+const delanteros = jugadores.filter(jugador => jugador.posicion === "DEL").sort(() => Math.random() - 0.5);
+
+equipo1.push(defensores[0], defensores[1], delanteros[0], delanteros[1]);
+equipo2.push(defensores[2], defensores[3], delanteros[2], delanteros[3]);
 
 console.table(equipo1);
 console.table(equipo2);
